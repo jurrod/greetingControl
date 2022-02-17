78f6e3d36604bdab77b62b7910ce02c01060a9ae
Luke James Charlesworth <vocatix@gmail.com>
2022-02-17 10:12:05 -0500

    Made input program and finished

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # greetingControl
-this is a project for lab 6 big ideas with Arjun, we do a little bit of greeting
-wassup arjun this is luke just testing commit 
+group members: Luke Charlesworth, Jarod Masarik 
+
+Our progam is very simple. It takes in three inputs from a college student: their name, age, and what university they attend. 
+By using a simple control flow structure it then compares the inputted age to the typical age of a student in each college grade year. 
+After determining what year the student is most likely in, the program prints a welcoming message to the console, stating that we hope their freshman || sophomore || junior || senior year is going great! 
--- partner.py
+++ partner.py
@@ -1,1 +1,17 @@
-jarrodIsCool = True;
+
+print("Hello all college students, prepare to answer some short questions")
+name = input("Please enter your name ");
+age = int(input("Now enter your age "));
+school = input("One last step, enter the university that you currently attend ");
+
+if (age == 18): 
+    print("hi", name, "I hope your freshman year at", school, "is going splendidly!");
+
+elif (age == 19):
+    print("hi", name, "I hope your sophomore year at",  school, "is going splendidly!");
+
+elif (age == 20):
+    print("hi", name, "I hope your junior year at", school, "is going splendidly!");
+
+else:
+    print("hi", name, "Congrats!! Finish this year up at", school, "and you will be a college grad!");
